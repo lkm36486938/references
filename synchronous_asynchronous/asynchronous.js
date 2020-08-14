96ed7ab630678a9ca267aaa489c85bed783e164f
lkm36486938 <ksan3940@gmail.com>
2020-08-14 09:30:10 +0900

    sync_async practice

---
+++ synchronous_asynchronous/asynchronous.js
@@ -0,0 +1,12 @@
+// 비동기
+var fs = require('fs');
+
+fs.readdir('.', function (err, filenames){  
+    var i;
+    for (i = 0; i < filenames.length; i++) {
+        console.log(filenames[i]);
+    }
+    console.log('ready');
+});
+
+console.log('can process next job...');  
